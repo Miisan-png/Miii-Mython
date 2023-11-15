674b75befb613e210e718e84d756a21d505e972f
Miisan <128460054+psy-lilulu@users.noreply.github.com>
2023-11-15 18:41:11 +0800

    Interpreter update

--- mii_interpreter.py
+++ mii_interpreter.py
@@ -1,40 +1,31 @@
-def interpret(filename):
-    # Create a dictionary to store variable values
-    variables = {}
+def tokenize(script):
+   return script.split("\n")
 
-    # Open the Mii script file
-    with open(filename, 'r') as file:
-        lines = file.readlines()
+def parse(token):
+   if token.startswith("mvar"):
+       var_name, var_value = token[5:].split(" = ")
+       globals()[var_name] = eval(var_value)
+   elif token.startswith("Add"):
+       var_name, var_value = token[4:].split(" to ")
+       globals()[var_name] += globals()[var_value]
+   elif token.startswith("mprint"):
+       print(eval(token[7:]))
+   elif token.startswith("mfunc"):
+       func_name, func_body = token[7:-1].split(":")
+       func_body = func_body.strip()
+       exec(f"def {func_name}(): {func_body}")
+   elif token.startswith("mrun"):
+       func_name = token[5:]
+       globals()[func_name]()
+   # Add more conditions here for other line types
 
-    # Process each line in the file
-    for line in lines:
-        # Split the line into words
-        words = line.split()
+def execute(script):
+   tokens = tokenize(script)
+   for token in tokens:
+       parse(token)
 
-        # Skip empty lines and comments
-        if len(words) == 0 or words[0].startswith('#'):
-            continue
+# Read the script from a file
+with open('script.my', 'r') as file:
+   script = file.read()
 
-        # Process Mii commands
-        elif words[0] == 'mvar':
-            # This is a variable assignment
-            variables[words[1]] = eval(words[3])
-        elif words[0] in ['Add', 'Sub', 'Multiply', 'Divide']:
-            # This is an arithmetic operation
-            if words[0] == 'Add':
-                variables[words[1]] = variables[words[1]] + variables[words[3]]
-            elif words[0] == 'Sub':
-                variables[words[1]] = variables[words[1]] - variables[words[3]]
-            elif words[0] == 'Multiply':
-                variables[words[1]] = variables[words[1]] * variables[words[3]]
-            elif words[0] == 'Divide':
-                variables[words[1]] = variables[words[1]] / variables[words[3]]
-        elif words[0] == 'mprint':
-            # This is a print statement
-            print(eval(words[1], variables))
-
-        else:
-            print(f"Unknown command: {words[0]}")
-
-if __name__ == "__main__":
-    interpret('test.my')
+execute(script)
