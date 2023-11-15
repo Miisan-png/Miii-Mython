a338c2ac26ff40ae9199e02307c35c7daa1102c8
Miisan <128460054+psy-lilulu@users.noreply.github.com>
2023-11-15 20:22:41 +0800

    Commit-

--- Demo.py
+++ Demo.py
@@ -1,53 +1,79 @@
-class Interpreter:
-    def __init__(self):
-        self.variables = {}
-        self.functions = {}
+# Custom language emulator in Python
 
-    def execute(self, script):
-        tokens = self.tokenize(script)
-        for token in tokens:
-            self.parse(token)
+# Dictionary to store variables
+variables = {}
+# Dictionary to store functions
+functions = {}
 
-    def parse(self, token):
-        # Using method dispatch instead of large if-elif chain
-        # This makes it easier to add more commands in the future
-        command = token.split(" ")[0]
-        method = getattr(self, command, None)
-        if method:
-            method(token)
-        else:
-            raise ValueError(f"Unknown command: {command}")
+# Custom language emulator in Python
 
-    def tokenize(self, script):
-        return script.split("\n")
+# Dictionary to store variables
+variables = {}
+# Dictionary to store functions
+functions = {}
 
-    def mvar(self, token):
-        var_name, var_value = token[5:].split(" = ")
-        self.variables[var_name] = eval(var_value, {}, self.variables)
+def run_command(command):
+    # Split the command into words
+    words = command.split()
 
-    def Add(self, token):
-        var_name, var_value = token[4:].split(" to ")
-        self.variables[var_name] += self.variables[var_value]
+    # Handle each command
+    if words[0] == 'mvar':
+        # Variable assignment
+        variables[words[1]] = eval(words[3])  # 'eval' is used to convert the string number to an integer
+    elif words[0] == 'mprint':
+        # Print command
+        to_print = ' '.join(words[1:])
+        print(variables[to_print]) if to_print in variables else print(to_print)
+    elif words[0] in ['Add', 'Sub', 'Multiply', 'Divide']:
+        # Arithmetic operations
+        var1 = variables[words[1]]
+        var2 = variables[words[3]]
+        if words[0] == 'Add':
+            variables[words[1]] = var1 + var2
+        elif words[0] == 'Sub':
+            variables[words[1]] = var1 - var2
+        elif words[0] == 'Multiply':
+            variables[words[1]] = var1 * var2
+        elif words[0] == 'Divide':
+            variables[words[1]] = var1 / var2
+    elif words[0] == 'mfunc':
+        # Function definition
+        func_name = words[1]
+        func_body = ' '.join(words[2:])
+        functions[func_name] = func_body
+    elif words[0] == 'mrun':
+        # Function call
+        func_name = words[1]
+        func_body = functions[func_name]
+        run_command(func_body)
+    elif words[0] == 'AskInput':
+        # User input
+        variables[words[1]] = input()
+    elif words[0] == 'Loop':
+        # Loop
+        loop_count = int(variables[words[1]])
+        loop_body = ' '.join(words[2:])
+        for _ in range(loop_count):
+            run_command(loop_body)
+    elif words[0] == 'If':
+        # If statement
+        var1 = variables[words[1]]
+        operator = words[2]
+        var2 = variables[words[3]]
+        if_body = ' '.join(words[4:])
+        if eval(f"{var1} {operator} {var2}"):
+            run_command(if_body)
 
-    def mprint(self, token):
-        print(eval(token[7:], {}, self.variables))
 
-    def mfunc(self, token):
-        func_name, func_body = token[7:-1].split(":")
-        self.functions[func_name] = func_body.strip()
-        print(f"Function {func_name} defined: {self.functions}")
+# Read the script file
+with open('script.my', 'r') as script:
+    for line in script:
+        # Remove newline character and any trailing spaces
+        line = line.strip()
 
-    def mrun(self, token):
-        func_name = token[5:]
-        print(f"Running function {func_name}: {self.functions}")
-        exec(self.functions[func_name], {}, self.variables)
+        # Ignore empty lines and comments
+        if line == '' or line.startswith('#'):
+            continue
 
-
-# Create an instance of the interpreter
-interpreter = Interpreter()
-
-# Read the script from a file
-with open('script.my', 'r') as file:
-    script = file.read()
-
-interpreter.execute(script)
+        # Run the command
+        run_command(line)
