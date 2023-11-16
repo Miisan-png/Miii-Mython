5def6c610cef3dde43250c1264a3d337fc25d3d8
Miisan <128460054+psy-lilulu@users.noreply.github.com>
2023-11-16 12:05:36 +0800

    Commit-7
    Shell update, added some a panel for running the language instead of manual py file.  Though intpr needs some changes for shell to work with.

--- Working-Build/Shell.py
+++ Working-Build/Shell.py
@@ -1,12 +1,13 @@
 # shell.py
 ## Importing the language
 import LanguageMi
+import glob
 
 print("Welcome to Mii Language Command Panel")
 print("Version 0.01")
 
 while True:
-    action = input('Would you like to run a script or create a new script? (run/create): ')
+    action = input('Would you like to run a script, create a new script, or edit an existing script? (run/create/edit): ')
     if action.lower() == 'run':
         filename = input('Enter the name of the script to run: ')
         with open(filename, 'r') as file:
@@ -25,5 +26,17 @@ while True:
                     break
                 file.write(line + '\n')
         print(f'Script {filename} created successfully.')
+    elif action.lower() == 'edit':
+        my_files = glob.glob('*.my')
+        print('Available .my files:', my_files)
+        filename = input('Enter the name of the .my script to edit: ')
+        with open(filename, 'a+') as file:
+            print(f'Start editing your script. Write "END" on a new line when you are done.\n')
+            while True:
+                line = input()
+                if line == 'END':
+                    break
+                file.write(line + '\n')
+        print(f'Script {filename} edited successfully.')
     else:
-        print('Invalid action. Please enter "run" or "create".')
+        print('Invalid action. Please enter "run", "create", or "edit".')
